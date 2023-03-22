bc391b23a3ff41bbcf8a82b3a8c19329951f0adc
fulgurance <zohran.londais@gmail.com>
2023-03-22 23:00:18 +0000

    Finished to implement Openrc installation properly

--- Openrc/0.46.0/0.46.0.cr
+++ Openrc/0.46.0/0.46.0.cr
@@ -24,10 +24,15 @@ class Target < ISM::Software
 
         runMesonCommand(["install"],buildDirectoryPath,
                                     {"DESTDIR" => "#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}"})
+
     end
 
     def install
         super
+
+        (1..6).each do |value|
+            makeLink("agetty","#{Ism.settings.rootPath}etc/init.d/agetty.tty#{value}",:symbolicLink)
+        end
 
         makeLink("openrc-init","#{Ism.settings.rootPath}sbin/init",:symbolicLink)
     end
