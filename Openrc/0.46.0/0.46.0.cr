b489e55e63708db69e0a01a79675798f184cf81f
Fulgurance <zohran.londais@gmail.com>
2024-05-05 14:17:56 +0100

    Updated symlink recording method

--- Openrc/0.46.0/0.46.0.cr
+++ Openrc/0.46.0/0.46.0.cr
@@ -28,17 +28,19 @@ class Target < ISM::Software
                             buildDirectoryPath,
                             {"DESTDIR" => "#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}"})
 
+        (1..6).each do |value|
+            makeLink("agetty","#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}etc/init.d/agetty.tty#{value}",:symbolicLink)
+        end
+
+        makeLink("openrc-init","#{builtSoftwareDirectoryPath(false)}#{Ism.settings.rootPath}sbin/init",:symbolicLink)
     end
 
     def install
         super
 
         (1..6).each do |value|
-            makeLink("agetty","#{Ism.settings.rootPath}etc/init.d/agetty.tty#{value}",:symbolicLink)
             runRcUpdateCommand(["add","agetty.tty#{value}","default"])
         end
-
-        makeLink("openrc-init","#{Ism.settings.rootPath}sbin/init",:symbolicLink)
     end
 
 end
