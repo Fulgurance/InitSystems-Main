ebd8bf0e1e5db58e6dbfcc4db201f2f4f333e03e
Fulgurance <zohran.londais@gmail.com>
2024-11-20 16:05:43 +0000

    Added proper installation process for openrc and inittab support

--- Openrc/0.52.1/0.52.1.cr
+++ Openrc/0.52.1/0.52.1.cr
@@ -28,19 +28,41 @@ class Target < ISM::Software
                         path:           buildDirectoryPath,
                         environment:    {"DESTDIR" => "#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}"})
 
-        makeDirectory("#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}sbin")
+        inittabData = <<-CODE
+        id:3:initdefault:
 
-        makeLink(   target: "openrc-init",
-                    path:   "#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}sbin/init",
-                    type:   :symbolicLink)
-    end
+        si::sysinit:/sbin/openrc sysinit
 
-    def install
-        super
+        rc::bootwait:/sbin/openrc boot
 
-        (1..6).each do |value|
-            runRcUpdateCommand(arguments:   "add agetty.tty#{value} default")
-        end
+        l0u:0:wait:/sbin/telinit u
+        l0:0:wait:/sbin/openrc shutdown
+        l0s:0:wait:/sbin/halt.sh
+        l1:1:wait:/sbin/openrc single
+        l2:2:wait:/sbin/openrc nonetwork
+        l3:3:wait:/sbin/openrc default
+        l4:4:wait:/sbin/openrc default
+        l5:5:wait:/sbin/openrc default
+        l6u:6:wait:/sbin/telinit u
+        l6:6:wait:/sbin/openrc reboot
+        l6r:6:wait:/sbin/reboot -dkn
+
+        su0:S:wait:/sbin/openrc single
+        su1:S:wait:/sbin/sulogin
+
+        c1:12345:respawn:/sbin/agetty --noclear 38400 tty1 linux
+        c2:2345:respawn:/sbin/agetty 38400 tty2 linux
+        c3:2345:respawn:/sbin/agetty 38400 tty3 linux
+        c4:2345:respawn:/sbin/agetty 38400 tty4 linux
+        c5:2345:respawn:/sbin/agetty 38400 tty5 linux
+        c6:2345:respawn:/sbin/agetty 38400 tty6 linux
+
+        ca:12345:ctrlaltdel:/sbin/shutdown -r now
+
+        x:a:once:/usr/bin/startDM
+
+        CODE
+        fileWriteData("#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}etc/inittab",inittabData)
     end
 
 end
